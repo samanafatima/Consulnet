e5ea0762cbfedcb4c8290deb08df6f519a418a35
samanafatima <samanafatima37@gmail.com>
2023-02-15 23:10:39 +0500

    secondcommit

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -48,6 +48,7 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
+ asdfghjklertyuiocvbnm,
 define( 'AUTH_KEY',         'put your unique phrase here' );
 define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
 define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
